40d42911f0e3a63e1eca3be219e95ebedefd0e04
ecamo19 <erickcamo19@gmail.com>
2024-03-25 20:21:44 +1100

    Adding parameter validation

--- R/2_droughtbox_plots.R
+++ R/2_droughtbox_plots.R
@@ -16,5 +16,26 @@
 #' plot_droughtbox_climatic_controls(droughtbox_data)
 
 plot_droughtbox_climatic_controls <- function(droughtbox_data, cowplot = TRUE){
-    NULL
+
+    # Validate input dataset ---------------------------------------------------
+
+    # Check that file exists and is not a folder
+    base::stopifnot("droughtbox_data should be a dataframe of type data.frame" =  "data.frame" %in% base::class(data))
+    base::stopifnot("Missing columns in the dataframe" =  c(
+                                                            "set_point_t_avg_avg",
+                                                            "set_point_vpd_avg_avg",
+                                                            "set_point_abs_h_avg_avg",
+                                                            "set_point_rh_avg_avg",
+
+                                                            "air_tc_avg_deg_c_avg",
+                                                            "rh_avg_percent_avg",
+                                                            "tc_avg_deg_c_avg",
+                                                            "vpd_avg_kpa_avg",
+                                                            "abs_h_avg_g_m3_avg"
+
+                                                            ) %in% base::colnames(data))
+
+
+
+
 }
