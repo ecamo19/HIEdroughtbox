4bb7df1baf7d3ea7bcba352119392855dc6b1387
ecamo19 <erickcamo19@gmail.com>
2024-03-25 16:03:59 +1100

    Working on tests

---
+++ tests/testthat/test-droughtbox_utils.R
@@ -0,0 +1,14 @@
+
+
+# Test for clean_droughtbox_colnames -------------------------------------------
+
+test_that("clean_droughtbox_colnames returns a object of type vector", {
+  expect_equal(clean_droughtbox_colnames(), 4)
+})
+
+
+
+
+# Test for read_hie_droughtbox_data --------------------------------------------
+
+
