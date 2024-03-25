c783b028073d9b11b80c0217ca0a8f78ae7b2e50
ecamo19 <erickcamo19@gmail.com>
2024-03-25 19:38:22 +1100

    Working in function

--- R/2_droughtbox_plots.R
+++ R/2_droughtbox_plots.R
@@ -1,1 +1,16 @@
-plot_droughtbox_climatic_conditions
+#' plot_droughtbox_climatic_controls
+#'
+#' @param droughtbox_data Data frame loaded with the function
+#' `read_hie_droughtbox_data`
+#'
+#' @de
+#' @param cowplot Boolean indicating if plots should be arranged in one single
+#' figure with 2 columns and 2 rows (TRUE) or if should be arranged individually
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_droughtbox_climatic_controls <- function(droughtbox_data, cowplot = TRUE){
+
+}
