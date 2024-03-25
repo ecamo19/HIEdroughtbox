84ad71f41ce42a6f7978fb97efbf36a191a9c7e3
ecamo19 <erickcamo19@gmail.com>
2024-03-25 19:45:55 +1100

    Setting branch

--- R/2_droughtbox_plots.R
+++ R/2_droughtbox_plots.R
@@ -1,16 +1,16 @@
 #' plot_droughtbox_climatic_controls
 #'
-#' @param droughtbox_data Data frame loaded with the function
+#' @param droughtbox_data Dataframe loaded with the function
 #' `read_hie_droughtbox_data`
 #'
-#' @de
+#' @importFrom magrittr %>%
 #' @param cowplot Boolean indicating if plots should be arranged in one single
 #' figure with 2 columns and 2 rows (TRUE) or if should be arranged individually
 #'
-#' @return
+#' @return A ggplot2 object with 4 figures
 #' @export
 #'
 #' @examples
 plot_droughtbox_climatic_controls <- function(droughtbox_data, cowplot = TRUE){
-
+    NULL
 }
