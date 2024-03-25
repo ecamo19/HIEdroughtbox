d724ca6d01fc1a19c6fafa37eb46db2ba92fe44e
ecamo19 <erickcamo19@gmail.com>
2024-03-25 19:56:16 +1100

    Minor change, example in skeleton updated

--- R/2_droughtbox_plots.R
+++ R/2_droughtbox_plots.R
@@ -4,13 +4,17 @@
 #' `read_hie_droughtbox_data`
 #'
 #' @importFrom magrittr %>%
+#'
 #' @param cowplot Boolean indicating if plots should be arranged in one single
 #' figure with 2 columns and 2 rows (TRUE) or if should be arranged individually
 #'
-#' @return A ggplot2 object with 4 figures
+#' @return A ggplot2 object with a total of 4 figures
 #' @export
 #'
 #' @examples
+#' droughtbox_data <- read_hie_droughtbox_data("data/acacia_aneura_25c.dat")
+#' plot_droughtbox_climatic_controls(droughtbox_data)
+
 plot_droughtbox_climatic_controls <- function(droughtbox_data, cowplot = TRUE){
     NULL
 }
