f485d43d4639aae6795a55c5b0dbea00214cd22b
ecamo19 <erickcamo19@gmail.com>
2024-03-26 17:21:49 +1100

    Test added

--- tests/testthat/test-droughtbox_plots.R
+++ tests/testthat/test-droughtbox_plots.R
@@ -1,1 +1,13 @@
 # Tests for plot_droughtbox_climatic_controls ----------------------------------
+test_that("Return object of type gg or ggplot", {
+    data <- read_hie_droughtbox_data("assets/droughtbox_output.dat")
+    expect_contains(class(plot_raw_strains_weights(data)), "ggplot")
+})
+
+
+# Tests for plot_raw_strains_weights -------------------------------------------
+test_that("Return object of type gg or ggplot", {
+    data <- read_hie_droughtbox_data("assets/droughtbox_output.dat")
+    expect_contains(class(plot_raw_strains_weights(data)), "ggplot")
+})
+
