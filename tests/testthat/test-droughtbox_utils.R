5eb46f109ba1d80c9d17cbe4a7ce709cda74ba90
ecamo19 <erickcamo19@gmail.com>
2024-04-04 14:37:26 +1100

    #6 Test added

--- tests/testthat/test-droughtbox_utils.R
+++ tests/testthat/test-droughtbox_utils.R
@@ -35,71 +35,98 @@ test_that("Return object of lenght 25 for clean_droughtbox_colnames", {
 # Tests for filter_hie_droughtbox_data -----------------------------------------
 data <- read_hie_droughtbox_data("assets/droughtbox_output.dat")
 
-test_that("Return an error when all parameters are set to NULL"){
-    expect_error(filter_hie_droughtbox_data(data = data,
+test_that("Return an error when all parameters are set to NULL",{
+    expect_error(filter_hie_droughtbox_data(droughtbox_data = data,
                                             from_start_date = NULL ,
                                             to_end_date = NULL,
                                             from_start_time = NULL,
-                                            to_end_time = NULL
-                                            ))
-}
+                                            to_end_time = NULL))
+})
 
-test_that("Return an error when one parameter is specified without the other"){
-    expect_error(filter_hie_droughtbox_data(data = data,
+test_that("Return an error when one parameter is specified without the other",{
+    expect_error(filter_hie_droughtbox_data(droughtbox_data = data,
                                             from_start_date = NULL ,
                                             to_end_date = NULL,
                                             from_start_time = "12:51:00",
-                                            to_end_time = NULL
-    ))
-}
+                                            to_end_time = NULL))
+})
 
 
-test_that("Return an error when one parameter is specified without the other"){
-    expect_error(filter_hie_droughtbox_data(data = data,
+test_that("Return an error when one parameter is specified without the other",{
+    expect_error(filter_hie_droughtbox_data(droughtbox_data = data,
                                             from_start_date = NULL,
                                             to_end_date = NULL,
                                             from_start_time = NULL,
-                                            to_end_time = "12:51:00"
-    ))
-}
+                                            to_end_time = "12:51:00"))
+})
 
 
-test_that("Return an error when one parameter is specified without the other"){
-    expect_error(filter_hie_droughtbox_data(data = data,
+test_that("Return an error when one parameter is specified without the other",{
+    expect_error(filter_hie_droughtbox_data(droughtbox_data = data,
                                             from_start_date = "2024-04-03" ,
                                             to_end_date = NULL,
                                             from_start_time = NULL,
-                                            to_end_time = NULL
-                                            ))
-}
+                                            to_end_time = NULL))
+})
 
 
-test_that("Return an error when one parameter is specified without the other"){
-    expect_error(filter_hie_droughtbox_data(data = data,
+test_that("Return an error when one parameter is specified without the other",{
+    expect_error(filter_hie_droughtbox_data(droughtbox_data = data,
                                             from_start_date = NULL,
                                             to_end_date = "2024-04-03",
                                             from_start_time = NULL,
-                                            to_end_time = NULL
-    ))
-}
+                                            to_end_time = NULL))
+})
 
 
-
-test_that("Return an error when one parameter is specified without the other"){
-    expect_error(filter_hie_droughtbox_data(data = data,
+test_that("Return an error when one parameter is specified without the other",{
+    expect_error(filter_hie_droughtbox_data(droughtbox_data = data,
                                             from_start_date = "2024-04-03" ,
                                             to_end_date = NULL,
                                             from_start_time = "12:51:00",
-                                            to_end_time = NULL
-    ))
-}
+                                            to_end_time = NULL))
+})
+
+test_that("Return an error when one parameter is specified without the other",{
+    expect_error(filter_hie_droughtbox_data(droughtbox_data = data,
+                                            from_start_date = "2024-04-03" ,
+                                            to_end_date = NULL,
+                                            from_start_time = "12:51:00",
+                                            to_end_time = NULL))
+})
+
+test_that("Return an error if time parameter is specified without secods",{
+    expect_error(filter_hie_droughtbox_data(droughtbox_data = data,
+                                            from_start_date = NULL,
+                                            to_end_date = NULL,
+                                            from_start_time = "12:51",
+                                            to_end_time = "12:52"))
+})
+
+test_that("Return 2 rows out of 796",{
+          expect_equal(nrow(filter_hie_droughtbox_data(droughtbox_data = data,
+                                                  from_start_date = NULL,
+                                                  to_end_date = NULL,
+                                                  from_start_time = "12:51:00",
+                                                  to_end_time = "12:52:00")), 2)
+})
+
+test_that("Return 796 rows out of 796",{
+    expect_equal(nrow(filter_hie_droughtbox_data(droughtbox_data = data,
+                                                 from_start_date = "2024/03/04",
+                                                 to_end_date = "2024/03/04",
+                                                 from_start_time = NULL,
+                                                 to_end_time = NULL)), 796)
+})
+
+test_that("Return 2 rows out of 796",{
+    expect_equal(nrow(filter_hie_droughtbox_data(droughtbox_data = data,
+                                                 from_start_date = "2024/03/04",
+                                                 to_end_date = "2024/03/04",
+                                                 from_start_time = "12:51:00",
+                                                 to_end_time = "12:52:00")), 2)
+})
 
 
-# filter_hie_droughtbox_data(data, from_start_time = "12:15:16" , to_end_time = NULL)
-# filter_hie_droughtbox_data(data, from_start_time = NULL , to_end_time = "12:15:16")
-#
-# filter_hie_droughtbox_data(data, from_start_date = NULL , to_end_date = "2024-03-04")
-# filter_hie_droughtbox_data(data, from_start_date = "2024-03-04", to_end_date = NULL)
-#
-# filter_hie_droughtbox_data(data, from_start_time = "12:15" , to_end_time = "12:15")
+
 # filter_hie_droughtbox_data(data, from_start_date = "2024/03/", to_end_date = "2024/03/")
