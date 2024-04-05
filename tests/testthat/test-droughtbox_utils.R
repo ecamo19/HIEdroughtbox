8dfbd97a8a3d32637d38482273028002da9186b4
ecamo19 <erickcamo19@gmail.com>
2024-04-05 17:31:20 +1100

    test for function clean_droughtbox_dataset added

--- tests/testthat/test-droughtbox_utils.R
+++ tests/testthat/test-droughtbox_utils.R
@@ -29,10 +29,10 @@ test_that("Return object of lenght 25 for clean_droughtbox_colnames", {
 })
 
 # Tests for filter_droughtbox_data -----------------------------------------
-data <- read_hie_droughtbox_data("assets/droughtbox_output.dat")
+test_data <- read_hie_droughtbox_data("assets/droughtbox_output.dat")
 
 test_that("Return an error when all parameters are set to NULL",{
-    expect_error(filter_droughtbox_data(droughtbox_data = data,
+    expect_error(filter_droughtbox_data(droughtbox_data = test_data,
                                             from_start_date = NULL ,
                                             to_end_date = NULL,
                                             from_start_time = NULL,
@@ -40,7 +40,7 @@ test_that("Return an error when all parameters are set to NULL",{
 })
 
 test_that("Return an error when one parameter is specified without the other",{
-    expect_error(filter_droughtbox_data(droughtbox_data = data,
+    expect_error(filter_droughtbox_data(droughtbox_data = test_data,
                                             from_start_date = NULL ,
                                             to_end_date = NULL,
                                             from_start_time = "12:51:00",
@@ -49,7 +49,7 @@ test_that("Return an error when one parameter is specified without the other",{
 
 
 test_that("Return an error when one parameter is specified without the other",{
-    expect_error(filter_droughtbox_data(droughtbox_data = data,
+    expect_error(filter_droughtbox_data(droughtbox_data = test_data,
                                             from_start_date = NULL,
                                             to_end_date = NULL,
                                             from_start_time = NULL,
@@ -58,7 +58,7 @@ test_that("Return an error when one parameter is specified without the other",{
 
 
 test_that("Return an error when one parameter is specified without the other",{
-    expect_error(filter_droughtbox_data(droughtbox_data = data,
+    expect_error(filter_droughtbox_data(droughtbox_data = test_data,
                                             from_start_date = "2024-04-03" ,
                                             to_end_date = NULL,
                                             from_start_time = NULL,
@@ -67,7 +67,7 @@ test_that("Return an error when one parameter is specified without the other",{
 
 
 test_that("Return an error when one parameter is specified without the other",{
-    expect_error(filter_droughtbox_data(droughtbox_data = data,
+    expect_error(filter_droughtbox_data(droughtbox_data = test_data,
                                             from_start_date = NULL,
                                             to_end_date = "2024-04-03",
                                             from_start_time = NULL,
@@ -76,7 +76,7 @@ test_that("Return an error when one parameter is specified without the other",{
 
 
 test_that("Return an error when one parameter is specified without the other",{
-    expect_error(filter_droughtbox_data(droughtbox_data = data,
+    expect_error(filter_droughtbox_data(droughtbox_data = test_data,
                                             from_start_date = "2024-04-03" ,
                                             to_end_date = NULL,
                                             from_start_time = "12:51:00",
@@ -84,7 +84,7 @@ test_that("Return an error when one parameter is specified without the other",{
 })
 
 test_that("Return an error when one parameter is specified without the other",{
-    expect_error(filter_droughtbox_data(droughtbox_data = data,
+    expect_error(filter_droughtbox_data(droughtbox_data = test_data,
                                             from_start_date = "2024-04-03" ,
                                             to_end_date = NULL,
                                             from_start_time = "12:51:00",
@@ -92,7 +92,7 @@ test_that("Return an error when one parameter is specified without the other",{
 })
 
 test_that("Return an error if time parameter is specified without secods",{
-    expect_error(filter_droughtbox_data(droughtbox_data = data,
+    expect_error(filter_droughtbox_data(droughtbox_data = test_data,
                                             from_start_date = NULL,
                                             to_end_date = NULL,
                                             from_start_time = "12:51",
@@ -100,7 +100,7 @@ test_that("Return an error if time parameter is specified without secods",{
 })
 
 test_that("Return 2 rows out of 796",{
-          expect_equal(nrow(filter_droughtbox_data(droughtbox_data = data,
+          expect_equal(nrow(filter_droughtbox_data(droughtbox_data = test_data,
                                                   from_start_date = NULL,
                                                   to_end_date = NULL,
                                                   from_start_time = "12:51:00",
@@ -108,7 +108,7 @@ test_that("Return 2 rows out of 796",{
 })
 
 test_that("Return 796 rows out of 796",{
-    expect_equal(nrow(filter_droughtbox_data(droughtbox_data = data,
+    expect_equal(nrow(filter_droughtbox_data(droughtbox_data = test_data,
                                                  from_start_date = "2024/03/04",
                                                  to_end_date = "2024/03/04",
                                                  from_start_time = NULL,
@@ -116,7 +116,7 @@ test_that("Return 796 rows out of 796",{
 })
 
 test_that("Return 2 rows out of 796",{
-    expect_equal(nrow(filter_droughtbox_data(droughtbox_data = data,
+    expect_equal(nrow(filter_droughtbox_data(droughtbox_data = test_data,
                                                  from_start_date = "2024/03/04",
                                                  to_end_date = "2024/03/04",
                                                  from_start_time = "12:51:00",
@@ -126,3 +126,56 @@ test_that("Return 2 rows out of 796",{
 # filter_droughtbox_data(data, from_start_date = "2024/03/", to_end_date = "2024/03/")
 
 # clean_droughtbox_dataset -----------------------------------------------------
+test_data <- read_hie_droughtbox_data("assets/droughtbox_output.dat")
+
+
+test_that("Return 1 row from a dataset with 13 rows",{
+    expect_equal(nrow(clean_droughtbox_dataset(test_data[11:23,],
+                                               remove_n_observations = 6)), 1)
+
+})
+
+test_that("Return 2 rows from a dataset with 14 rows",{
+    expect_equal(nrow(clean_droughtbox_dataset(test_data[11:24,],
+                                               remove_n_observations = 6)), 2)
+
+})
+
+test_that("Return 3 rows from a dataset with 15 rows",{
+    expect_equal(nrow(clean_droughtbox_dataset(test_data[11:25,],
+                                               remove_n_observations = 6)), 3)
+
+})
+
+test_that("Return 5 rows from a dataset with 15 rows",{
+    expect_equal(nrow(clean_droughtbox_dataset(test_data[11:25,],
+                                               remove_n_observations = 5)), 5)
+
+})
+
+test_that("Return 30 rows from a dataset with 50 rows",{
+    expect_equal(nrow(clean_droughtbox_dataset(test_data[11:60,],
+                                               remove_n_observations = 10)), 30)
+
+})
+
+
+
+test_that("Return 60 rows from a dataset with 50 rows",{
+    expect_equal(nrow(clean_droughtbox_dataset(test_data[11:110,],
+                                               remove_n_observations = 10)), 59)
+
+})
+
+
+
+test_that("Return error when tare groups don't have enough tares",{
+    expect_error(nrow(clean_droughtbox_dataset(test_data[1:13,],
+                                               remove_n_observations = 6)))
+
+})
+
+
+
+
+
