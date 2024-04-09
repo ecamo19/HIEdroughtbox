5a2ca32bfacadea43bf3f03dee3e38615cc497d2
ecamo19 <erickcamo19@gmail.com>
2024-04-09 18:45:27 +1000

    #5 Initial commit for gres function

---
+++ R/03_calculate_minimum_conductance.R
@@ -0,0 +1,33 @@
+
+#' residual_conductance
+#'
+#' @description
+#' This
+#'
+#' @param droughtbox_data
+#'
+#' @param leaf_and_stem_area
+#'
+#' @importFrom magrittr %>%
+#'
+#' @return
+#'
+#' @examples
+#'
+#' @export
+residual_conductance <- function(droughtbox_data,
+                                 leaf_and_stem_area
+                                 ){
+
+    # Validate input dataset ---------------------------------------------------
+
+    # Calculate leaf residual conductance --------------------------------------
+
+    ## Get the slope between the measured weight loss and time -----------------
+
+    ## Estimate transpiration --------------------------------------------------
+    #e <- -(slope/(total_leaf_area_m2 + branch_surface_area_m2))
+
+    ## Estimate leaf residual conductance --------------------------------------
+
+}
