274d6257d9ecf1845299c7bc194173b75876d737
ecamo19 <erickcamo19@gmail.com>
2025-01-06 19:00:40 +1100

    Working on Implementing RWC function

--- R/04_calculate_relative_water_content.R
+++ R/04_calculate_relative_water_content.R
@@ -14,3 +14,22 @@
 #        return((strain_weight_g - dry_weight_g) / (turgid_weight - dry_weight_g))
 #
 #}
+
+#' Relative water content
+#'
+#' @description
+#' This function calculates the relative water content of each sample.
+#'
+#'
+#' @param droughtbox_data Dataframe loaded with the function
+#' `read_hie_droughtbox_data()`.
+#'
+#' @param dry_weights Dataframe loaded with the functionv `read_dry_weights_data()`
+#'
+#' @return
+#' @export
+#'
+#' @examples
+relative_water_content <- function(droughtbox_data, dry_weights){
+
+}
